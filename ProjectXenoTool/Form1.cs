e8ff32e57014b9e8b1c7a08ea71da2eb4a17e9c3
Tyty <54728263+80b@users.noreply.github.com>
2020-11-14 04:27:27 -0500

    Create Form1.cs

---
+++ ProjectXenoTool/Form1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectXenoTool
+{
+  
+  public partial class Form1 : MetroFramework.Forms.MetroForm
+  {
+    
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    
+    
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      
+    }
+  }
+}
